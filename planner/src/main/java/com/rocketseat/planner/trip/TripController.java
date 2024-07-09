e4fce4a871d6e18e5bcebf16fcf9eb0d08250eb6
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-07-09 11:21:43 -0300

    Creating a Request to show Trip Details

--- planner/src/main/java/com/rocketseat/planner/trip/TripController.java
+++ planner/src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -2,10 +2,10 @@ package com.rocketseat.planner.trip;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/trips")
@@ -15,11 +15,19 @@ public class TripController {
     private TripRepository repository;
 
     @PostMapping
-    public ResponseEntity<String> createTrip(@RequestBody TripRequestPayLoad payLoad){
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayLoad payLoad){
         Trip newTrip = new Trip(payLoad);
 
         this.repository.save(newTrip);
 
-        return ResponseEntity.ok("Sucesso!");
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+        Optional<Trip> trip = this.repository.findById(id);
+
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 }
